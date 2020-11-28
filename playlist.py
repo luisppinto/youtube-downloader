d1639d6383add50ba42b06a36678d4369a2abb0a
luisppinto <72820660+luisppinto@users.noreply.github.com>
2020-11-28 14:22:20 +0000

    First commit.

---
+++ playlist.py
@@ -0,0 +1,15 @@
+import pytube
+
+'''WARNING: DOWNLOADING COPYRIGHTED MATERIAL IS HIGHLY ILLEGAL!
+I DO NOT TAKE ANY RESPONSIBILITY FROM THE USAGE OF THIS TOOL!
+THIS IS FOR EDUCATIONAL PURPOSES ONLY!'''
+
+url = input("Paste Playlist URL")
+
+playlist = pytube.Playlist(url)
+for url in playlist:
+    video = pytube.Youtube(url)
+    stream = video.streams.get_by_itag(22)
+    print("Downloading playlist...")
+    stream.download
+    print("Done!")
---
+++ video.py
@@ -0,0 +1,21 @@
+import pytube
+
+'''WARNING: DOWNLOADING COPYRIGHTED MATERIAL IS HIGHLY ILLEGAL!
+I DO NOT TAKE ANY RESPONSIBILITY FROM THE USAGE OF THIS TOOL!
+THIS IS FOR EDUCATIONAL PURPOSES ONLY!'''
+
+video_list = []
+
+print("Enter URLs (Terminate by 'STOP')")
+while True:
+    url = input("")
+    if url == 'STOP':
+        break
+    video_list.append(url)
+
+for x, video in enumerate(video_list):
+    v = pytube.Youtube(video)
+    stream = v.streams.get_by_itag(22)
+    print(f"Downloading video {x}..."
+    stream.download()
+    print("Done")
